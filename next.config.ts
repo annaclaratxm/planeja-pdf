ef6ea3c1b938507363b471cd01eaac6ccdcebb40
Oziel Silveira Junior <37221417+ozielsilveira@users.noreply.github.com>
2024-12-02 14:49:38 -0300

    refactor: Update image domain configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: {
         domains: [
-            process.env.CLOUDFLARE_R2_ENDPOINT_PUBLIC?.replace(/^https?:\/\//, '').replace('/planeja-pdf', '') || ''
+            process.env.CLOUDFLARE_R2_ENDPOINT?.replace(/^https?:\/\//, '').replace('/planeja-pdf', '') || ''
         ],
     },
 };
