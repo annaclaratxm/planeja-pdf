4660653988430a14761883d4879beba321489950
Oziel Silveira Junior <37221417+ozielsilveira@users.noreply.github.com>
2024-12-03 02:00:26 -0300

    refactor: Update PDF generation styles
    Simplify and improve the styles for generating PDFs in the budget-generate-pdf.tsx file. The padding has been reduced to 30, the font size has been increased to 12, and the font family has been changed to Helvetica.

--- src/app/client/budget/components/budget-generate-pdf.tsx
+++ src/app/client/budget/components/budget-generate-pdf.tsx
@@ -3,23 +3,13 @@
 
 import { generatePdf } from '@/services/api/pdf-generator/actions';
 import { GetFileFromR2 } from '@/services/bucket/cloudflare';
-import { Document, Font, Image, Page, StyleSheet, Text, View, pdf } from '@react-pdf/renderer';
-
-Font.register({
-    family: 'Roboto',
-    fonts: [
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf', fontWeight: 300 },
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf', fontWeight: 400 },
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-medium-webfont.ttf', fontWeight: 500 },
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf', fontWeight: 700 },
-    ],
-});
+import { Document, Image, Page, StyleSheet, Text, View, pdf } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
     page: {
-        padding: 40,
-        fontSize: 10,
-        fontFamily: 'Roboto',
+        padding: 30,
+        fontSize: 12,
+        fontFamily: 'Helvetica',
         backgroundColor: '#FFFFFF',
         color: '#333333',
     },
